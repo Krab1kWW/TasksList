339773ca0534cc4f158ec98e832d591cda0b3361
Dmitry <da76316363@gmail.com>
2026-08-12 02:44:44 +0400

    Update script.js
    Доделал тз2

--- script.js
+++ script.js
@@ -38,7 +38,8 @@ function  addTask()
 function showTasks(filter)
     {
         let all = tasks.length
-        let completed = 
+        let completed = tasks.filter(t => t.completed).length
+        let active = tasks.filter(t => !t.completed).length
         console.log(`Всего:${all}| Выполнено: ${completed}| Активные:${active} `)
         if (filter == 'all')
             {
@@ -143,16 +144,16 @@ function getValidTaskNumber()
         let index = num - 1
         if (isNaN(num))
             {
-                alert("Введите число!")
+                return null
             }
         else if (index < 0 || index >= tasks.length)
             {
-                alert("Задачи с таким номером нет!")
+                return null
 
             }
         else
         {
-            console.log(tasks[index])
+            return index
         }
     }
 
@@ -161,6 +162,72 @@ function printTask(task, number)
         let completed = task.completed ? 'x' : ' '
         let text = `${number}. [${completed}] ${task.text} `;              
         console.log(text)                    
+    }
+
+function editTask()
+    {
+        let index = getValidTaskNumber()
+
+        if (index != null)
+            {
+                let newText = prompt("Введите новый текст:")
+                if (newText == null || newText.trim() === "")
+                    {
+                        alert("Задача не может быть пустой")
+                    }
+                else
+                    {
+                        tasks[index].text = newText;
+                        console.log("Задача обновлена!");
+                    }
+            }
+    }
+
+function clearCompleted()
+    {
+        let completedTasks = tasks.filter(t => t.completed);
+        let completedTasksLength = completedTasks.length
+        if (completedTasks.length == 0)
+        {
+            console.log("Нет выполненных задач для удаления")
+
+        }
+        else
+        {
+            tasks = tasks.filter(t => t.completed === false);
+            let completed = `Удалено ${completedTasksLength}!`;          
+            console.log(completed)
+        }
+
+    }
+
+function searchTask()
+    {
+        let text = prompt("Введите текст задачи:")
+        let lowText = text.toLowerCase()
+        if (text == null || text.trim() === "")
+            {
+                alert("Введите текст.")
+            }
+        else  
+        {
+            let counter = 1
+            for(let i = 0; i<tasks.length; i++)
+            {
+                let taskText = tasks[i].text
+                let taskTextLow = taskText.toLowerCase()
+                if (taskTextLow.includes(lowText))
+                {
+                    printTask(tasks[i], counter)
+                    counter++
+                }                             
+            }
+            if(counter === 1)
+            {
+                console.log("Задачи не найдены")
+            }
+        }
+        
     }
 
 function exit()
@@ -206,22 +273,24 @@ while (status)
                     break;
 
                 case '7' : // Редактировать задачу
-                    
+                    editTask()
                     break;
 
                 case '8' : //  Удалить все выполненные
-                    
+                    clearCompleted()
                     break;
 
                 case '9' : // Найти задачу по тексту
-               
+                    searchTask()
                     break;
-
 
                 case '0' : // Выход
                     exit()
                     break;
-                    
+                
+                default:
+                    alert("Неизвестная команда! Введите число от 0 до 9.");
+                    break;
 
 
             }
